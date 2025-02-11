39c51deec73f60c0065794575bd4ebe01084bc49
lorematteo <loremattteo@gmail.com>
2025-02-11 16:33:41 +0100

    fix(ai-prompt): switch from next public var to private env variable

--- src/config/mistral.tsx
+++ src/config/mistral.tsx
@@ -1,6 +1,6 @@
 import { Mistral } from '@mistralai/mistralai';
 
-const apiKey = process.env.NEXT_PUBLIC_MISTRAL_API_KEY;
+const apiKey = process.env.MISTRAL_API_KEY;
 
 const client = new Mistral({ apiKey: apiKey });
 
