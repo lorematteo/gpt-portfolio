3a2491654158dd324537d0c23adaa1eba76d4129
lorematteo <loremattteo@gmail.com>
2025-02-12 16:13:29 +0100

    evol(cards-gallery): creating piano card

--- src/features/card-gallery/index.tsx
+++ src/features/card-gallery/index.tsx
@@ -1,13 +1,14 @@
 import { PROJECTS } from '@/const/projects';
 
 import Card from './components/card';
+import PianoCard from './components/cards/piano';
 import ProjectCard from './components/cards/project';
 
 const LeftCardsGallery = () => {
   return (
     <div className="grid w-full max-w-xl grid-cols-6 gap-4 overflow-y-hidden px-4 max-xl:hidden">
       <ProjectCard project={PROJECTS['f1-nft-card-packs']} />
-      <Card className="col-span-6" />
+      <PianoCard />
       <div className="col-span-4 flex flex-col gap-4">
         <Card size="lg" />
         <Card />
