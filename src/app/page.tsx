2fbe156d74797480a32c40dbe913dd4629737e8c
lorematteo <loremattteo@gmail.com>
2025-02-14 19:39:30 +0100

    evol(cards-gallery): removing hscreen style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LeftCardsGallery, RightCardsGallery } from '@/features/card-gallery';
 
 export default function HomePage() {
   return (
-    <div className="flex h-screen w-full flex-row items-center justify-center pt-14 xl:justify-between">
+    <div className="flex w-full flex-row items-center justify-center pb-4 pt-14 xl:justify-between">
       <LeftCardsGallery />
       <AiPrompt />
       <RightCardsGallery />
