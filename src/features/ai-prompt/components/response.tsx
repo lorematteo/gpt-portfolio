d6733754dd95a7ed55367cd70bed4f38251c8dba
lorematteo <loremattteo@gmail.com>
2025-02-16 15:33:37 +0100

    evol(ai-prompt): handling markdown response

--- src/features/ai-prompt/components/response.tsx
+++ src/features/ai-prompt/components/response.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 import { ChatContext } from '@/app/provdiers';
 
@@ -7,7 +8,7 @@ const Response: React.FC = () => {
   return (
     <div className="absolute top-14 z-50 flex max-h-52 w-full px-4 sm:w-[42rem]">
       <div className="w-full flex-col items-center justify-center overflow-y-auto rounded-3xl border border-light-gray bg-gray-100 px-4 py-3 dark:border-neutral-gray dark:bg-medium-gray">
-        <p className="text-sm">{loading ? "I'm thinking ..." : response}</p>
+        <ReactMarkdown className="text-sm">{loading ? "I'm thinking ..." : response}</ReactMarkdown>
       </div>
     </div>
   );
